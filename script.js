0da73ea67f716b306f055531742bb0e534457d75
Acacio Souza da Silva <zadfcer666@gmail.com>
2024-06-04 14:32:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const contador = document.querySelector('#contador');
+
+
+
+const timer = ()=>{
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const data = new Date('07/01/2024 00:00').getTime();
+    let agora = new Date(Date.now()).getTime();
+    const diff = Math.floor((data - agora)/day);
+    return `Faltam ${diff} dias.` ;
+}
+
+
+contador.innerText = timer();
