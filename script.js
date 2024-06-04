948fafd51aa21e924858e00da8296014bbd949cd
Acacio Souza da Silva <zadfcer666@gmail.com>
2024-06-04 14:46:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const timer = ()=>{
     const data = new Date('07/01/2024 00:00').getTime();
     let agora = new Date(Date.now()).getTime();
     const diff = Math.floor((data - agora)/day);
-    return `Faltam ${diff} dias.` ;
+    return `Faltam ${diff} dias` ;
 }
 
 
