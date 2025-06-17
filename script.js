daa6a7a13f331cfd1766754135dfd206c8891a3e
Acacio Souza da Silva <zadfcer666@gmail.com>
2025-06-17 08:00:52 -0300

    Update script.js
    alterado para o formato mm/dd/yyyy

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const timer = ()=>{
     const hour = minute * 60;
     const day = hour * 24;
 
-    const data = new Date('30/06/2025').getTime();
+    const data = new Date('06/30/2025').getTime();
     let agora = new Date(Date.now()).getTime();
     const diff = Math.floor((data - agora)/day);
     return `Faltam ${diff+1} dias` ;
